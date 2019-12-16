fa6f3420235a30a5bce86c95e068c93bf236633a
adrian <d00194311@student.dkit.ie>
2019-12-16 09:04:31 +0000

    Blah5

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -21,7 +21,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import okhttp3.Call;
--- app/src/main/java/com/example/clicker/ShopActivity.java
+++ app/src/main/java/com/example/clicker/ShopActivity.java
@@ -2,19 +2,28 @@ package com.example.clicker;
 
 import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static com.example.clicker.BaseApplication.CHANNEL_1_ID;
+import static com.example.clicker.BaseApplication.CHANNEL_2_ID;
 
 public class ShopActivity extends AppCompatActivity {
 
     private DatabaseManager dbManager;
+    private NotificationManagerCompat notificationManager;
 
     Button item1;
     Button item2;
@@ -22,7 +31,14 @@ public class ShopActivity extends AppCompatActivity {
     Button item4;
     Button item5;
 
+    Button testBtn;
+
     TextView currCountView;
+    ProgressBar progressBar1;
+    ProgressBar progressBar2;
+    ProgressBar progressBar3;
+    ProgressBar progressBar4;
+    ProgressBar progressBar5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,8 @@ public class ShopActivity extends AppCompatActivity {
         dbManager = new DatabaseManager(this);
         dbManager.open();
 
+        notificationManager = NotificationManagerCompat.from(this);
+
         final Session session = new Session(getApplicationContext());
 
         item1 = findViewById(R.id.item1);
@@ -43,6 +61,20 @@ public class ShopActivity extends AppCompatActivity {
         item4 = findViewById(R.id.item4);
         item5 = findViewById(R.id.item5);
         currCountView = findViewById(R.id.currCountShopView);
+
+        testBtn = findViewById(R.id.testBtn);
+
+        progressBar1 = findViewById(R.id.progressBar1);
+        progressBar2 = findViewById(R.id.progressBar2);
+        progressBar3 = findViewById(R.id.progressBar3);
+        progressBar4 = findViewById(R.id.progressBar4);
+        progressBar5 = findViewById(R.id.progressBar5);
+
+        progressBar1.setProgress(currCount);
+        progressBar2.setProgress(currCount);
+        progressBar3.setProgress(currCount);
+        progressBar4.setProgress(currCount);
+        progressBar5.setProgress(currCount);
 
         currCountView.setText("Balance = " + String.valueOf(currCount));
 
@@ -64,11 +96,21 @@ public class ShopActivity extends AppCompatActivity {
             item5.setBackgroundColor(Color.parseColor("#ff1111"));
         }
 
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hi");
+                sendOnChannel1("Purchased X2 Multiplier!");
+            }
+        });
+
         item1.setOnClickListener(new View.OnClickListener() {
             int price = 100;
             int newMultiplier = 2;
             @Override
             public void onClick(View v) {
+                sendOnChannel1("Purchased X2 Multiplier!");
+
                 if(items[0] == 0) {
                     if (currCount > price) {
                         dbManager.buyItem(session, newMultiplier, price, 1);
@@ -167,5 +209,33 @@ public class ShopActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void sendOnChannel1(String message) {
+        String title = "Keep Going!";
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    public void sendOnChannel2(String message) {
+        String title = "Keep Going!";
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+
+        notificationManager.notify(2, notification);
     }
 }
